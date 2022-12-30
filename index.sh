193b90dedfa8b4c6e22cd2d15a01c69a5f24a988
wdsk <wdsk@wdskdeMac-Pro.local>
2022-12-30 14:10:41 +0800

    docs

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">' > index.html
-echo '<style> a { display: block; margin-top: 0.7em; } </style>' >> index.html
+echo '<style> .nav-item { margin-top: 0.7em; } </style>' >> index.html
 
 for file in `ls *.html | tr " " "\?"`
 do
@@ -11,5 +11,5 @@ do
 	then
 		continue
 	fi
-	echo "<a href=\"$file\">$file</a>" >> index.html
+	echo "<div class=\"nav-item\"><a href=\"$file\">$file</a></div>" >> index.html
 done
